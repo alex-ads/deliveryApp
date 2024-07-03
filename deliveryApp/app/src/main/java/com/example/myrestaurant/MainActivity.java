6562e94e16f20c416d278a30e17a0fc6aa48913d
Alex <126741430+alex-ads@users.noreply.github.com>
2024-07-02 22:40:33 -0300

    Add files via upload

---
+++ deliveryApp/app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myrestaurant;
+
+//TODO fix back button issues
+//TODO
+//TODO
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    
+    ListView listView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        listView = findViewById(R.id.main);
+        
+        listView.setAdapter(new MenuAdapter(this, Database.categories));
+        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+                Intent toFoodList = new Intent(getApplicationContext(), FoodListActivity.class);
+                toFoodList.putExtra("category", Database.categories.get(position).getCategory());
+                startActivity(toFoodList);
+            }
+        });
+    }
+    
+}
